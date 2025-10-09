8738544932926044fd1d94bd9ac6da13209c4b7e
AnderAzpiazu <148545080+AnderAzpiazu@users.noreply.github.com>
2025-10-09 10:29:20 +0200

    main.py

--- GitHub_Actions/main.py
+++ GitHub_Actions/main.py
@@ -3,3 +3,9 @@ import os
 class kalkulagailua:
     def batu(self, a: int, b: int) -> int:
         return a + b
+    
+def main():
+    print("Docker github actions")    
+    
+if __name__ == "__main__":
+    main()
--- GitHub_Actions/test.py
+++ GitHub_Actions/test.py
@@ -1,4 +1,9 @@
 from main import kalkulagailua
 
+
+def test_batu():
+    assert kalkulagailua().batu(2, 3) == 5
+from main import kalkulagailua
+
 def test_batu():
     assert kalkulagailua().batu(2, 3) == 5
